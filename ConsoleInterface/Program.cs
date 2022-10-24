00383ecec36b849cb792f03fbba27cd2bf5fa808
rzymski <83671265+rzymski@users.noreply.github.com>
2022-10-24 04:55:34 +0200

    Upgrade classes and methods

--- ConsoleInterface/Program.cs
+++ ConsoleInterface/Program.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleInterface
 {
-    public class MiniSquare
+    public class MiniSquareSymbolFragment
     {
         public int column { get; set; }
         public int row { get; set; }
-        public MiniSquare()
+        public MiniSquareSymbolFragment()
         {
             column = 0;
             row = 0;
         }
-        public MiniSquare(int column, int row)
+        public MiniSquareSymbolFragment(int column, int row)
         {
             this.column = column;
             this.row = row;
         }
     }
 
-    class Program
+    class Draw
     {
-        static void drawRectangle(int startColumn, int startRow, int height, int width, int colorNumber)
+        public static void drawRectangle(int startColumn, int startRow, int height, int width, int colorNumber)
         {
             Console.ForegroundColor = (ConsoleColor)colorNumber;
             Console.SetCursorPosition(startColumn, startRow);
@@ -47,26 +47,23 @@ namespace ConsoleInterface
                 Console.Write("═");
             Console.Write("╝");
         }
-
-        static void drawSquare(int startColumn, int startRow, int sizeSquare, int colorNumber)
+        public static void drawSquare(int startColumn, int startRow, int sizeSquare, int colorNumber)
         {
             drawRectangle(startColumn, startRow, sizeSquare, sizeSquare, colorNumber);
         }
-
-        static void errorOccurred(string communicat)
+        public static void errorOccurred(string communicat)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
-            Console.SetCursorPosition(40,25);
+            Console.SetCursorPosition(40, 25);
             Console.WriteLine("Wystapil blad:");
             Console.SetCursorPosition(40, 26);
             Console.WriteLine(communicat);
             Console.ReadKey();
             Environment.Exit(-1);
         }
-
-        static void uploadParametrs(int arenaSize, ref int squareSize)
+        public static void uploadParametrs(int arenaSize, ref int squareSize)
         {
             if (arenaSize == 3)
                 squareSize = 12;
@@ -79,19 +76,34 @@ namespace ConsoleInterface
                 errorOccurred("Podano niewlasciwa wartosc arenaSize. Wcisnij dowolny przycisk zeby zakonczyc program.\n");
             }
         }
-
-        static void drawArena(int arenaSize, int startColumn=50, int startRow=5)
+        public static void drawArena(int arenaSize, int startColumn = 50, int startRow = 5)
         {
             int squareSize = 10;
             uploadParametrs(arenaSize, ref squareSize);
-            for(int i=0; i<arenaSize; i++)
+            for (int i = 0; i < arenaSize; i++)
             {
                 for (int j = 0; j < arenaSize; j++)
-                    drawSquare(startColumn+i*squareSize, startRow+j*squareSize ,squareSize, 15);
+                    drawSquare(startColumn + i * squareSize, startRow + j * squareSize, squareSize, 15);
             }
         }
 
-        static void drawSymbolSize1(int startColumn, int startRow, int symbolValue, int colorNumber)
+        public static void drawSymbol(int option, int startColumn, int startRow, int symbolValue, int colorNumber)
+        {
+            switch (option)
+            {
+                case 1:
+                    drawSymbolSize1(startColumn, startRow, symbolValue, colorNumber);
+                    break;
+                case 2:
+                    drawSymbolSize5(startColumn, startRow, symbolValue, colorNumber);
+                    break;
+                case 3:
+                    drawSymbolSize10(startColumn, startRow, symbolValue, colorNumber);
+                    break;
+            }
+        }
+
+        public static void drawSymbolSize1(int startColumn, int startRow, int symbolValue, int colorNumber)
         {
             Console.ForegroundColor = (ConsoleColor)colorNumber;
             char symbol = ' ';
@@ -102,7 +114,7 @@ namespace ConsoleInterface
             Console.SetCursorPosition(startColumn, startRow);
             Console.Write(symbol);
         }
-        static void drawSymbolSize5(int startColumn, int startRow, int symbolValue, int colorNumber)
+        public static void drawSymbolSize5(int startColumn, int startRow, int symbolValue, int colorNumber)
         {
             Console.ForegroundColor = (ConsoleColor)colorNumber;
             if (symbolValue == 1)
@@ -110,53 +122,53 @@ namespace ConsoleInterface
                 Console.BackgroundColor = (ConsoleColor)colorNumber;
                 Console.SetCursorPosition(startColumn, startRow);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+1, startRow+1);
+                Console.SetCursorPosition(startColumn + 1, startRow + 1);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+2, startRow+2);
+                Console.SetCursorPosition(startColumn + 2, startRow + 2);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+1, startRow+3);
+                Console.SetCursorPosition(startColumn + 1, startRow + 3);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn, startRow+4);
+                Console.SetCursorPosition(startColumn, startRow + 4);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+4, startRow);
+                Console.SetCursorPosition(startColumn + 4, startRow);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+3, startRow+1);
+                Console.SetCursorPosition(startColumn + 3, startRow + 1);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+3, startRow+3);
+                Console.SetCursorPosition(startColumn + 3, startRow + 3);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+4, startRow+4);
+                Console.SetCursorPosition(startColumn + 4, startRow + 4);
                 Console.Write(" ");
             }
-            else if(symbolValue == 0)
+            else if (symbolValue == 0)
             {
                 Console.BackgroundColor = (ConsoleColor)colorNumber;
-                Console.SetCursorPosition(startColumn, startRow+1);
+                Console.SetCursorPosition(startColumn, startRow + 1);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn, startRow+2);
+                Console.SetCursorPosition(startColumn, startRow + 2);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn, startRow+3);
-                Console.Write(" ");
-
-                Console.SetCursorPosition(startColumn+4, startRow + 1);
-                Console.Write(" ");
-                Console.SetCursorPosition(startColumn+4, startRow + 2);
-                Console.Write(" ");
-                Console.SetCursorPosition(startColumn+4, startRow + 3);
+                Console.SetCursorPosition(startColumn, startRow + 3);
                 Console.Write(" ");
 
+                Console.SetCursorPosition(startColumn + 4, startRow + 1);
+                Console.Write(" ");
+                Console.SetCursorPosition(startColumn + 4, startRow + 2);
+                Console.Write(" ");
+                Console.SetCursorPosition(startColumn + 4, startRow + 3);
+                Console.Write(" ");
 
-                Console.SetCursorPosition(startColumn+1, startRow);
+
+                Console.SetCursorPosition(startColumn + 1, startRow);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+2, startRow);
+                Console.SetCursorPosition(startColumn + 2, startRow);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+3, startRow);
+                Console.SetCursorPosition(startColumn + 3, startRow);
                 Console.Write(" ");
 
-                Console.SetCursorPosition(startColumn+1, startRow+4);
+                Console.SetCursorPosition(startColumn + 1, startRow + 4);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+2, startRow+4);
+                Console.SetCursorPosition(startColumn + 2, startRow + 4);
                 Console.Write(" ");
-                Console.SetCursorPosition(startColumn+3, startRow+4);
+                Console.SetCursorPosition(startColumn + 3, startRow + 4);
                 Console.Write(" ");
 
             }
@@ -166,13 +178,12 @@ namespace ConsoleInterface
             }
             Console.BackgroundColor = ConsoleColor.Black;
         }
-
-        static void drawSymbolSize10(int startColumn, int startRow, int symbolValue, int colorNumber)
+        public static void drawSymbolSize10(int startColumn, int startRow, int symbolValue, int colorNumber)
         {
-            MiniSquare[] points = new MiniSquare[100];
-            for(int i=0; i<10; i++)
-                for(int j=0; j<10; j++)
-                    points[i*10+j] = new MiniSquare(startColumn+j, startRow+i);
+            MiniSquareSymbolFragment[] points = new MiniSquareSymbolFragment[100];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    points[i * 10 + j] = new MiniSquareSymbolFragment(startColumn + j, startRow + i);
 
             Console.BackgroundColor = (ConsoleColor)colorNumber;
             if (symbolValue == 1)
@@ -184,7 +195,7 @@ namespace ConsoleInterface
                     Console.Write(" ");
                 }
             }
-            else if(symbolValue == 0)
+            else if (symbolValue == 0)
             {
                 int[] tabCircle = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 20, 30, 40, 50, 60, 70, 80, 91, 92, 93, 94, 95, 96, 97, 98, 19, 29, 39, 49, 59, 69, 79, 89 };
                 for (int i = 0; i < tabCircle.Length; i++)
@@ -199,70 +210,106 @@ namespace ConsoleInterface
             }
             Console.BackgroundColor = ConsoleColor.Black;
         }
+    }
 
-        static void Main(string[] args)
+    class CircleAndCross
+    {
+        private int option;
+        private int startRow;
+        private int startColumn;
+        private int squareSize;
+        private int symbolValue;
+        private int colorValue;
+
+
+        private void initializeParameters(int option)
         {
-            Console.CursorVisible = false;
-            Console.WindowHeight = 50;
-            Console.WindowWidth = 100;
+            symbolValue = 1; //krzyz
+            colorValue = 12; //czerwony
+            switch (option)
+            {
+                case 1:
+                    Draw.drawArena(13);
+                    startRow = 68;
+                    startColumn = 23;
+                    squareSize = 3;
+                    break;
+                case 2:
+                    Draw.drawArena(5);
+                    startRow = 64;
+                    startColumn = 19;
+                    squareSize = 7;
+                    break;
+                case 3:
+                    Draw.drawArena(3);
+                    startRow = 62;
+                    startColumn = 17;
+                    squareSize = 12;
+                    break;
+            }
+        }
 
-
-
-            drawArena(13);
-            int w = 68, k = 23, ss = 3;
-
-            /*drawArena(5);
-            int w = 64, k = 19, ss = 7;*/
-
-            /*drawArena(3); //12, 5, 3
-            int w = 62, k = 17, ss = 12;*/
-            int v = 0, cv = 15;
-
+        public CircleAndCross(int option)
+        {
+            this.option = option;
+            initializeParameters(option);
+        }
+        public void gameplay()
+        {
             ConsoleKey key;
             do
             {
                 while (!Console.KeyAvailable)
                 {
-                    // Do something, but don't read key here
-                    drawSquare(w, k, ss, 12);
+                    Draw.drawSquare(startRow, startColumn, squareSize, colorValue); //12 = red
                     System.Threading.Thread.Sleep(250);
-                    drawSquare(w, k, ss, 15);
+                    Draw.drawSquare(startRow, startColumn, squareSize, 15); //15 = white
                     System.Threading.Thread.Sleep(250);
                 }
                 // Key is available - read it
                 key = Console.ReadKey(true).Key;
                 if (key == ConsoleKey.Enter || key == ConsoleKey.Spacebar)
                 {
-                    v = (v + 1) % 2;
-                    if (v == 1)
-                        cv = 12;
+                    Draw.drawSymbol(option, startRow + 1, startColumn + 1, symbolValue, colorValue);
+                    if (symbolValue == 0)
+                        colorValue = 12; //zmiana koloru na czerwony
                     else
-                        cv = 9;
-                    //drawSymbolSize10(w+1, k+1, v, cv);
-                    //drawSymbolSize5(w+1, k+1, v, cv);
-                    drawSymbolSize1(w + 1, k + 1, v, cv);
+                        colorValue = 9; //zmiana koloru na niebieski
+                    symbolValue = (symbolValue + 1) % 2; //zmiana symbolu
                 }
                 else if (key == ConsoleKey.LeftArrow || key == ConsoleKey.A || key == ConsoleKey.NumPad4)
                 {
-                    w -= ss;
+                    startRow -= squareSize;
                 }
                 else if (key == ConsoleKey.RightArrow || key == ConsoleKey.D || key == ConsoleKey.NumPad6)
                 {
-                    w += ss;
+                    startRow += squareSize;
                 }
                 else if (key == ConsoleKey.UpArrow || key == ConsoleKey.W || key == ConsoleKey.NumPad8)
                 {
-                    k -= ss;
+                    startColumn -= squareSize;
                 }
                 else if (key == ConsoleKey.DownArrow || key == ConsoleKey.S || key == ConsoleKey.NumPad2)
                 {
-                    k += ss;
+                    startColumn += squareSize;
                 }
-
             } while (key != ConsoleKey.Escape);
-
+            //After escape set cursor at 0,0 and print "leftArrow"
             Console.SetCursorPosition(0, 0);
             Console.WriteLine(ConsoleKey.LeftArrow.ToString());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 100;
+
+            CircleAndCross c = new CircleAndCross(2);
+            c.gameplay();
 
             for (int i=0; i<10; i++)
                 Console.WriteLine();
